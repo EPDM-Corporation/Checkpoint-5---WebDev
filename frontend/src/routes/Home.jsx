d3e61fd22974740ff960da3aa116a2ff1d66741d
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-05-09 15:04:34 -0300

    Instalando fontes do google fonts e aplicando no sobre

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -12,8 +12,8 @@ const Home = () => {
         <div className='border-5 h-[629px]'>
             <img src={SobreImage} alt="" />
             <div>
-                <h2>Sobre o CycloVolt</h2>
-                <p>No Cyclovolt promovemos a mobilidade elétrica sustentável com bicicletas
+                <h2 className="font-bebas text-[64px]">Sobre o CycloVolt</h2>
+                <p className='font-montserrat text-[20px]'>No Cyclovolt promovemos a mobilidade elétrica sustentável com bicicletas
                 modernas, ecológicas e de alto desempenho. Nosso foco é facilitar o transporte
                 urbano com mais praticidade, saúde e consciência ambiental.
                 Acreditamos que pedalar com uma bike elétrica é um passo simples para um futuro

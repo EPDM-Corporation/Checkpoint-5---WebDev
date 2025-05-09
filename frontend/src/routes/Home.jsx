3b0ea41e2844d2e64e1107372470f3e4bdf4e602
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-05-09 15:19:21 -0300

    Criando a estrutura para os cards da home

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -9,22 +9,24 @@ const Home = () => {
     <div>
         <Banner></Banner>
         {/* SOBRE */}
-        <div className='border-5 h-[629px] flex gap-[10px] justify-around items-center'>
+        <section className='h-[629px] flex  justify-around items-center'>
             <img src={SobreImage} alt="" className='w-[550px] h-[550px]' />
             <div>
                 <h2 className="font-bebas text-[64px]">Sobre o CycloVolt</h2>
-                <p className='font-montserrat text-[20px] w-[564px]'>No Cyclovolt promovemos a mobilidade elétrica sustentável com bicicletas
+                <p className='font-montserrat text-[20px] w-[524px]'>No Cyclovolt promovemos a mobilidade elétrica sustentável com bicicletas
                 modernas, ecológicas e de alto desempenho. Nosso foco é facilitar o transporte
                 urbano com mais praticidade, saúde e consciência ambiental.
                 Acreditamos que pedalar com uma bike elétrica é um passo simples para um futuro
                 com menos trânsito e mais qualidade de vida. Venha pedalar com propósito!</p>
             </div>
-        </div>
+        </section>
 
         {/* CARDS */}
-        <div className='border-5 h-[632px]'>
-
-        </div>
+        <section className='border-5 h-[632px] bg-[#F3F3F3]'>
+            <h1>
+                Mais Vendidos
+            </h1>
+        </section>
     </div>
   )
 }

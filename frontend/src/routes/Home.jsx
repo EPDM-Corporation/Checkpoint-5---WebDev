4546a0b4d48749fcd1609ec7d73b3476a8b903a7
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-05-09 14:12:46 -0300

    Adicionando imagem e texto ao sobre home

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Home.css'
 import Banner from '../components/Banner/Banner'
+import SobreImage from '../assets/sobreimage.png'
+
 
 const Home = () => {
   return (
@@ -8,7 +10,15 @@ const Home = () => {
         <Banner></Banner>
         {/* SOBRE */}
         <div className='border-5 h-[629px]'>
-
+            <img src={SobreImage} alt="" />
+            <div>
+                <h2>Sobre o CycloVolt</h2>
+                <p>No Cyclovolt promovemos a mobilidade elétrica sustentável com bicicletas
+                modernas, ecológicas e de alto desempenho. Nosso foco é facilitar o transporte
+                urbano com mais praticidade, saúde e consciência ambiental.
+                Acreditamos que pedalar com uma bike elétrica é um passo simples para um futuro
+                com menos trânsito e mais qualidade de vida. Venha pedalar com propósito!</p>
+            </div>
         </div>
 
         {/* CARDS */}

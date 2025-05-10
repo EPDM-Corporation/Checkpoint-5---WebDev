40277b0fc1bb1bf987f943585ed5a6086ea681f4
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-05-10 14:43:32 -0300

    Criando o design da página contatos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import {BrowserRouter,Routes, Route} from 'react-router-dom'
 import Home from './routes/Home'
 import Error from './routes/Error'
+import Contatos from './routes/Contatos'
+
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 
@@ -13,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='*' element={<Error/>}/>
+          <Route path='/contatos' element={<Contatos/>}/>
         </Routes>
       </BrowserRouter>
     <Footer></Footer>

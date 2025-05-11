3abacaf51a05d059726190a26d318b5f0dc0ca66
Lucas <lguerreiroms@gmail.com>
2025-05-11 20:57:49 -0300

    Criando o Header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Header.css'
+import logo from '../../assets/logo.png'
 
 const Header = () => {
   return (
     <div className='border-5 h-[189px]'>
-        asd
+        <div className="menu">
+          <img src={logo} id="logo" />
+          <div className="botoes">
+            <h1 id="home">Home</h1>
+            <h1>Bicicletas</h1>
+            <h1>Contato</h1>
+            <h1>carrinho </h1>
+            <h1>homem</h1>
+          </div>
+        </div>
     </div>
   )
 }

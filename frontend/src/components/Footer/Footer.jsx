f641bc1c42bb706faa5c738bdf210c1e5e20ecda
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-05-11 09:04:14 -0300

    feat:adicionando o font-awesome no projeto react

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,41 +1,26 @@
 import React from "react";
 import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="social-section">
-        <p>SIGA NOSSAS REDE SOCIAIS</p>
-        <div className="social-icons">
-          <a href="#" aria-label="Facebook">
-            <i className="fab fa-facebook-f"></i>
-          </a>
-          <a href="#" aria-label="Instagram">
-            <i className="fab fa-instagram"></i>
-          </a>
-          <a href="#" aria-label="LinkedIn">
-            <i className="fab fa-linkedin-in"></i>
-          </a>
-          <a href="#" aria-label="YouTube">
-            <i className="fab fa-youtube"></i>
-          </a>
+    <footer className="border-5 h-[402px]">
+      {/* FOOTER TOP */}
+      <div className="bg-[#F0D250] h-[88px]">
+        <h2 className="font-bebas text-[36px]">Siga nossas redes sociais</h2>
+        <div>
+          <FontAwesomeIcon icon={faFacebook} size="3x"/>
+          <FontAwesomeIcon icon={faLinkedin} size="3x" />
+          <FontAwesomeIcon icon={faInstagram} size="3x"/>
+          <FontAwesomeIcon icon={faYoutube}  size="3x"/>
         </div>
+
       </div>
-      <div className="footer-content">
-        <div className="footer-left">
-          <h3>CYCLOVOLT</h3>
-        </div>
-        <div className="footer-right">
-          <ul>
-            <li><a href="#">Termos de serviço</a></li>
-            <li><a href="#">Cookies</a></li>
-            <li><a href="#">Políticas de Privacidade</a></li>
-          </ul>
-        </div>
-      </div>
-      <div className="footer-bottom">
-        <p>© 2025 CycloVolt. Todos os direitos reservados.</p>
-      </div>
+
     </footer>
   );
 };

cdbc39e9e84f299c680bf7f6f50c065a4e21f0da
Guilherme Vital Rodrigues <guivitalrodrigues@gmail.com>
2025-05-11 00:29:48 -0300

    Finalizando o header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,28 @@
-import {BrowserRouter,Routes, Route} from 'react-router-dom'
-import Home from './routes/Home'
-import Error from './routes/Error'
-import Contatos from './routes/Contatos'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Error from './routes/Error';
+import Contatos from './routes/Contatos';
 
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
+// Importando o Font Awesome localmente
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   return (
     <>
-    <Header></Header>
+      <Header />
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='*' element={<Error/>}/>
-          <Route path='/contatos' element={<Contatos/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Error />} />
+          <Route path="/contatos" element={<Contatos />} />
         </Routes>
       </BrowserRouter>
-    <Footer></Footer>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
